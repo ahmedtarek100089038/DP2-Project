31028981b04db9cde1f873b2c618a651bef59720
tixlee <55526379+tixlee@users.noreply.github.com>
2020-05-07 01:02:27 +0800

    Update craftpophouse.sql

--- database/craftpophouse.sql
+++ database/craftpophouse.sql
@@ -45,7 +45,8 @@ Create TABLE `seller` (
 
 Insert INTO `seller` (`id`, `name`, `email`, `password`, `is_active`) VALUES
 (1, 'Lu', 'lu@gmail.com', '1234567890abc', '1'),
-(2, 'Simon', 'simon@gmail.com', '1234567890abcd', '0'); 
+(2, 'Simon', 'simon@gmail.com', '1234567890abcd', '0'),
+(3, 'Jason', 'jason@gmail.com', '1234567890abcd', '0');
 
 -- --------------------------------------------------------
 
@@ -125,7 +126,9 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`order_id`, `user_id`, `product_id`, `qty`, `trx_id`, `p_status`) VALUES
 (1, 1, 1, 1, '9L434522M7706801A', 'Completed'),
-(2, 1, 3, 1, '9L434523M7706801A', 'Completed');
+(2, 1, 3, 1, '9L434523M7706801A', 'Completed'),
+(3, 1, 3, 1, '8L434523M7706801B', 'Completed')
+(4, 1, 4, 2, '7L434523M7706801A', 'Completed');
 
 -- --------------------------------------------------------
 
@@ -152,10 +155,52 @@ CREATE TABLE `products` (
 INSERT INTO `products` (`product_id`, `product_cat`, `product_brand`, `product_title`, `product_price`, `product_qty`, `product_desc`, `product_image`, `product_keywords`) VALUES
 (1, 1, 1, 'Pink Checkered Skirt', 50, 100, 'Pink Checkered Skirt', 'clothing1.jpg', 'pink, checkered, skirt'),
 (2, 1, 1, 'Womens Purple Dress', 70, 100, 'Womens Purple Dress', 'clothing2.jpg', 'womens, purple, dress'),
-(3, 1, 1, 'Mens Patterned Tee', 55, 100, 'Tradisional Patterned White Tee', 'clothing3.jpg', 'mens, patterned, tee'),
-(4, 1, 1, 'Mens Formal Shirt', 125, 100, 'Formal Shirt with Stars Design', 'clothing4.jpg', 'mens, formal, shirt'),
-(5, 1, 1, 'Mens Cotton Shirt', 79, 100, 'Small Red Couple Figured Cotton Shirt', 'clothing5.jpg', 'mens, cotton, shirt'),
-(6, 1, 1, 'Short Sleeves Fringed Shirt', 99, 100, 'Tradition Tribal Design', 'clothing6.jpg', 'short, sleeves, fringed, shirt');
+(3, 1, 1, 'Mens Patterned White Tee', 55, 100, 'Tradisional Patterned White Tee', 'clothing3.jpg', 'mens, white, patterned, tee'),
+(4, 1, 1, 'Mens Mismatched Flannel', 125, 100, 'New blue & red design', 'clothing4.jpg', 'mens, mismatched, flannel'),
+(5, 1, 1, 'Mens Figured Cotton Shirt', 79, 100, 'Small Red Couple Figured Cotton Shirt', 'clothing5.jpg', 'mens, red, cotton, shirt'),
+(6, 1, 1, 'Short Sleeves Fringed Shirt', 99, 100, 'Tradition Tribal Design', 'clothing6.jpg', 'short, sleeves, fringed, shirt'),
+(7, 1, 1, 'Black & White Striped Dress', 99, 100, 'Black & White officer wear', 'clothing7.jpg', 'black, white, officer, wear'),
+(8, 1, 1, 'Polka Dotted Red Dress', 79, 100, 'Modern Polka Dotted Dress', 'clothing8.jpg', 'modern, polka, dotted, dress'),
+(9, 2, 1, 'Indian Handmade Jewellery', 12, 100, 'Green Orange Jewellery', 'jewellery1.jpg', 'green, indian, orange, handmade, jewellery'),
+(10, 2, 1, 'Indian Handmade Jewellery', 12, 100, 'Light Green Fuchsia Pink', 'jewellery2.jpg', 'indian, handmade, jewellery'),
+(11, 2, 1, 'Handmade 925 Sterling Silver', 149, 100, 'Oval shaped with "X" design', 'jewellery3.jpg', 'oval, shaped, with, x'),
+(12, 2, 1, 'Handmade 925 Sterling Silver', 129, 100, 'Heart shaped design', 'jewellery4.jpg', 'heart, shaped, design'),
+(13, 2, 1, 'Tassel Beaded Bracelet', 99, 100, 'Jet Black Design', 'jewellery5.jpg', 'jet, black, design'),
+(14, 2, 1, 'Vintage Silver Necklace', 179, 100, 'Sherlock Holmes Design', 'jewellery6.jpg', 'sherlock, holmes, design'),
+(15, 2, 1, 'Beaded Cuffs Jewellery', 39, 100, 'Diamond Necklaces with gift', 'jewellery7.jpg', 'diamond, necklace, with, gift'),
+(16, 2, 1, 'Tagua Nut Necklaces', 129, 100, 'Special Designed Necklaces', 'jewellery7.jpg', 'special, designed, necklaces'),
+(17, 3, 1, 'Lion Soft Toy', 25, 100, 'Lion', 'softtoy1.jpg', 'lion, soft, toy'),
+(18, 3, 1, 'Monster Inc Toy', 35, 100, 'Mile Wazowski', 'softtoy2.jpg', 'monster, inc, toy, mike, wazowski'),
+(19, 3, 1, 'Stuffed Koala Bear', 75, 100, 'Feels like Australia', 'softtoy3.jpg', 'feels, like, australia, koala, bear'),
+(20, 3, 1, 'Kitty Cat', 30, 100, 'Stuffed Pink Cute Cat', 'softtoy4.jpg', 'stuffed, pink, cute, cat'),
+(21, 3, 1, 'Stithch and Angel Set', 99, 100, 'Cute Stuffed Soft Toy Set', 'softtoy5.jpg', 'stitch, angel, set'),
+(22, 3, 1, 'Stuffed Penguin', 39, 100, 'Cute Little Penguin', 'softtoy6.jpg', 'cute, little, penguins'),
+(23, 3, 1, 'Pokemon Soft Toy', 27, 100, 'Pikachu', 'softtoy7.jpg', 'pokemon, soft, toy, pikachu'),
+(24, 3, 1, 'Handmade Fox Toy', 79, 100, 'Limited Edition', 'softtoy8.jpg', 'handmade, fox, toy'),
+(25, 4, 1, 'Handmade Rattan Basket', 50, 100, 'Different colours to choose from', 'handmade1.jpg', 'handmade, rattan, basket'),
+(26, 4, 1, 'Handmade Crayon Bookmark', 15, 100, 'Made by young children from age 7 - 10', 'handmade2.jpg', 'handmade, crayon, bookmark'),
+(27, 4, 1, 'Handmade Patterned Basket', 75, 100, 'Beautiful coloured mosaic design', 'handmade3.jpg', 'handmade, patterned, basket'),
+(28, 4, 1, 'Handmade cup holder', 25, 100, 'Knitted special sunflower design', 'handmade4.jpg', 'handmade, cup, holder'),
+(29, 4, 1, 'Handmade Animal Figurine', 89, 100, 'Wood-Crafted Animal Figurine', 'handmade5.jpg', 'handmade, wood, animal, figurine'),
+(30, 4, 1, 'Handmade Wooden Plates', 15, 100, 'Minimal hand carved nature designs', 'handmade6.jpg', 'minimal, hand, carved, nature, design'),
+(31, 4, 1, 'Handmade Cylindrical Basket', 39, 100, 'Long strap design for shoulder carrying', 'handmade7.jpg', 'handmade, cylindrical, basket'),
+(32, 4, 1, 'Handmade Spherical Gift Box', 25, 100, 'Coloured Romantic GIft Box for loved ones', 'handmade8.jpg', 'handmade, spherical, gift, card'),
+(33, 5, 1, 'Feather Crafts', 20, 100, 'Home Decoration Feather', 'decor1.jpg', 'home, decoration, feather, crafts'),
+(34, 5, 1, 'Wine bottle craft', 40, 100, 'Wine bottle craft decoration', 'decor2.jpg', 'wine, bottle, craft'),
+(35, 5, 1, 'Tulle Roll', 10, 100, 'Tulle Roll 25Yard', 'decor3.jpg', 'Tulle, Roll, 25Yard'),
+(36, 5, 1, 'LED Decoration', 54, 100, 'Homemade LED Decoration', 'decor4.jpg', 'Homemade LED Decoration'),
+(37, 5, 1, 'Vase and Jar Set Decor', 25, 100, 'Home Decoration Vase & Jar Set', 'decor5.jpg', 'home, decoration, vase, jar'),
+(38, 5, 1, 'Flower Ceiling Decor', 30, 100, 'Handmade flower design decor', 'decor6.jpg', 'flower, ceiling, decor'),
+(39, 5, 1, 'Wooden Designed Vase', 10, 100, 'Authentic Nature Home Decor', 'decor7.jpg', 'authentic, nature, home, decor'),
+(40, 5, 1, 'Hanging Transparent Vase', 60, 100, 'Colourful polka dots outer layer design', 'decor8.jpg', 'colourful, polka, dots, outer, layer, design'),
+(41, 6, 1, 'Vintage Camera', 499, 100, 'Vintage Camera from 1955', 'vintage1.jpg', 'vintage, camera, 1955'),
+(42, 6, 1, 'Vintage Watch', 780, 100, 'Vintage Watch from 1942', 'vintage2.jpg', 'vintage, watch, 1942'),
+(43, 6, 1, 'CD Player', 450, 100, 'CD player from 1966', 'vintage3.jpg', 'cd, player, 1966'),
+(44, 6, 1, 'Gramophone', 199, 100, 'Gramophone from 1945', 'vintage4.jpg', 'gramophone, 1945'),
+(45, 6, 1, 'Vintage Lamp', 99, 100, 'Lamp from year 1978', 'vintage5.jpg', 'lamp, year, 1978'),
+(46, 6, 1, 'Gramophone', 780, 100, 'Gramiphone 1955 Vintage', 'vintage6.jpg', 'vintage, gramophone, 1955'),
+(47, 6, 1, 'Vivitar Camera', 450, 100, 'Camera from year 1977', 'vintage7.jpg', 'vivitar, camera, 1977'),
+(48, 6, 1, 'Golden Wheel', 99, 100, 'Golden wheel collection', 'vintage8.jpg', 'golden, wheel, collection');
 
 -- --------------------------------------------------------
 
@@ -180,7 +225,10 @@ CREATE TABLE `user_info` (
 
 INSERT INTO `user_info` (`user_id`, `first_name`, `last_name`, `email`, `password`, `mobile`, `address1`, `address2`) VALUES
 (1, 'Simon', 'Lu', 'simonlu@gmail.com', '1234567890abc', '0198008888', 'Simpang Tiga', '93300'),
-(2, 'Xavier', 'Lee', 'xavierlee@gmail.com', '1234567890abcd', '0190800991', 'Simpang Tiga', '93300');
+(2, 'Xavier', 'Lee', 'xavierlee@gmail.com', '1234567890abcd', '0190800991', 'Simpang Tiga', '93300'),
+(3, 'Tommy', 'Lim', 'tommylim@gmail.com', '1234567890abcde', '0190880991', 'Simpang Tiga', '93300'),
+(4, 'Jenny', 'Chua', 'jennychua@gmail.com', '1234567890abcdf', '0190801992', 'Simpang Tiga', '93300'),
+(5, 'Arthur', 'Lee', 'arthurlee@gmail.com', '1234567890abcdg', '0190830991', 'Simpang Tiga', '93300');
 
 --
 -- Indexes for dumped tables
@@ -249,7 +297,7 @@ ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
   
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table `seller`
 --
 ALTER TABLE `seller`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
