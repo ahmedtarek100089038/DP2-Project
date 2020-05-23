e0123c7ec79d230134e22b8b5687d3dbe8c4401e
ahmedtarek100089038 <55531401+ahmedtarek100089038@users.noreply.github.com>
2020-05-23 19:52:54 +0800

    User's Review in Admin Panel

---
+++ admin/user_review.php
@@ -0,0 +1,55 @@
+<?php session_start(); ?>
+<?php include_once("./templates/top.php"); ?>
+<?php include_once("./templates/navbar.php"); ?>
+
+<div class="container-fluid">
+  <div class="row">
+    
+    <?php include "./templates/sidebar.php"; ?>
+
+	<div class="row">
+      	<div class="col-10">
+      		<h2>User's Review</h2>
+      	</div>
+      </div>
+	  
+	  <hr>
+	  
+ <?php 
+
+  require 'config.php';
+   if (isset($_SESSION['admin_id'])) {
+          $userLoggedIn = $_SESSION['admin_id'];
+          $result = mysqli_query($con,"SELECT * FROM tbl_comment");
+
+            echo "<table class='table table-striped table-sm'>
+            <tr>
+            <th>Name</th>
+            <th>Riview</th>
+            <th>Date</th>
+            </tr>";
+
+            while($row = mysqli_fetch_array($result))
+            {
+            echo "<tr>";
+            echo "<td>" . $row['comment_sender_name'] . "</td>";
+            echo "<td>" . $row['comment'] . "</td>";
+            echo "<td>" . $row['date'] . "</td>";
+            echo "</tr>";
+            }
+            echo "</table>";                                                                    
+}
+  else {
+  //header("Location: index.php");
+  }
+
+
+
+
+   ?>
+    
+</div>
+</div>
+  
+<?php include "./templates/footer.php"; ?>
+
