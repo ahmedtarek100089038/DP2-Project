899dce0e6c0998847063e9b577d03fdabdff7730
tixlee <55526379+tixlee@users.noreply.github.com>
2020-05-25 21:05:32 +0800

    Update navbar.php

--- admin/templates/navbar.php
+++ admin/templates/navbar.php
@@ -1,8 +1,12 @@
  <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
   <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Craft Pop House</a>
   
+  
+  <p style="color:white;margin-top:1%;">Welcome: <?php echo $_SESSION["admin_name"]; ?></p>
   <ul class="navbar-nav px-3">
+   
     <li class="nav-item text-nowrap">
+	
     	<?php
     		if (isset($_SESSION['admin_id'])) {
     			?>
