e4dafad4c4347cecb955c462a8a8c88ccddb6240
ahmedtarek100089038 <55531401+ahmedtarek100089038@users.noreply.github.com>
2020-05-26 23:07:17 +0800

    Updated customer page in Admin panel

--- admin/customers.php
+++ admin/customers.php
@@ -12,106 +12,45 @@
       	</div>
       </div>
       
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
+      <?php 
+
+  require 'config.php';
+   if (isset($_SESSION['admin_id'])) {
+          $userLoggedIn = $_SESSION['admin_id'];
+          $result = mysqli_query($con,"SELECT first_name, last_name, email, mobile, address1 FROM user_info");
+
+            echo "<table class='table table-striped table-sm'>
             <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Mobile</th>
-              <th>Address</th>
-            </tr>
-          </thead>
-          <tbody id="customer_list">
-            <!-- <tr>
-              <td>Customer1</td>
-              <td>Customer2</td>
-              <td>Customer3</td>
-              <td>Customer4</td>
-              <td>Customer5</td>
-              <td>Customer6</td>
-              <td><a class="btn btn-sm btn-info"></a><a class="btn btn-sm btn-danger">Delete</a></td>
-            </tr> -->
-          </tbody>
-        </table>
-      </div>
-    </main>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Address</th>
+            </tr>";
+
+            while($row = mysqli_fetch_array($result))
+            {
+            echo "<tr>";
+            echo "<td>" . $row['first_name'] . "</td>";
+            echo "<td>" . $row['last_name'] . "</td>";
+            echo "<td>" . $row['email'] . "</td>";
+            echo "<td>" . $row['mobile'] . "</td>";
+            echo "<td>" . $row['address1'] . "</td>";
+            echo "</tr>";
+            }
+            echo "</table>";                                                                    
+}
+  else {
+  //header("Location: index.php");
+  }
+
+
+
+
+   ?>
   </div>
 </div>
 
-
-
-<!-- Modal -->
-<div class="modal fade" id="add_product_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form id="add-product-form" enctype="multipart/form-data">
-        	<div class="row">
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Name</label>
-		        		<input type="text" name="product_name" class="form-control" placeholder="Enter Product Name">
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Brand Name</label>
-		        		<select class="form-control brand_list" name="brand_id">
-		        			<option value="">Select Brand</option>
-		        		</select>
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Category Name</label>
-		        		<select class="form-control category_list" name="category_id">
-		        			<option value="">Select Category</option>
-		        		</select>
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Description</label>
-		        		<textarea class="form-control" name="product_desc" placeholder="Enter product desc"></textarea>
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Price</label>
-		        		<input type="number" name="product_price" class="form-control" placeholder="Enter Product Price">
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Keywords <small>(eg: shirt, dress, craft)</small></label>
-		        		<input type="text" name="product_keywords" class="form-control" placeholder="Enter Product Keywords">
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Image <small>(format: jpg, jpeg, png)</small></label>
-		        		<input type="file" name="product_image" class="form-control">
-		        	</div>
-        		</div>
-        		<input type="hidden" name="add_product" value="1">
-        		<div class="col-12">
-        			<button type="button" class="btn btn-primary add-product">Add Product</button>
-        		</div>
-        	</div>
-        	
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
 <!-- Modal -->
 
 <?php include_once("./templates/footer.php"); ?>
